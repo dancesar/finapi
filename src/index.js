6679e8402c591334ceec888ddcc5a843ad838961
root <root@DESKTOP-FSLCI01.localdomain>
2021-04-07 09:27:18 -0300

    Implementado novas funcionalidades.

--- src/index.js
+++ src/index.js
@@ -39,10 +39,27 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement/", verifyIfExistAccountCPF, (request, response) => {
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
     const { customer } = request;
 
     return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        create_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(3333);
